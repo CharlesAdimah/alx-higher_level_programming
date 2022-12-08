6584063ebfee5cdbc27f2957219388c43debb047
CharlesAdimah <charlesosejob@gmail.com>
2022-12-08 05:41:59 -0800

    1-search_replace.py

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,14 +1,21 @@
 #!/usr/bin/python3
 
-def uniq_add(my_list=[]):
+def search_replace(my_list, search, replace):
     """
-    sum the unique elements of my_list
+    search and replace an element in a list
+    Args:
+        my_list - The list to search
+        search - element to replace
+        replace - subtitute for search
     """
     if my_list is None:
         return None
-    return sum(set(my_list))
+    return [
+        replace if x == search else x for x in my_list
+    ]
 
 if __name__ == '__main__':
-    my_list = [1, 2, 3, 1, 4, 2, 5]
-    result = uniq_add(my_list)
-    print("Result: {:d}".format(result))
+    my_list = [1, 2, 3, 4, 5, 4, 2, 1, 1, 4, 89]
+    new_list = search_replace(my_list, 2, 89)
+    print(my_list)
+    print(new_list)
